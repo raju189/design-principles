a75903d3305d964d4eaf16754c6c89225ccfb368
Raju <raju1894u@gmail.com>
2020-04-02 17:45:24 +0530

    Update Windows98Machine.java

--- src/solid/dependency/inversion/principle/Windows98Machine.java
+++ src/solid/dependency/inversion/principle/Windows98Machine.java
@@ -4,7 +4,12 @@ package solid.dependency.inversion.principle;
  * The principle of Dependency Inversion refers to the decoupling of software
  * modules. This way, instead of high-level modules depending on low-level
  * modules, both will depend on abstractions.
- * 
+ * High Level modules should not depend on low-level modules. Both should depend on abstaction.
+* 		Never depend on anything concrete, only depend on abstraction.
+*
+ *		Able to change an implementation easily without altering the high level code.
+
+ 
  * @author G_DisGGreat
  *
  */
